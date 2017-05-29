39e4cc398d289927a5ab0914127ab1b7aacee5a8
AlessandroPatti <ale812@yahoo.it>
2017-05-29 11:01:05 +0200

    lab 1.3: sys_read and sys_write (fix)

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -116,6 +116,7 @@ syscall(struct trapframe *tf)
 				 	(void*) tf->tf_a1,
 					(size_t) tf->tf_a2,
 					&retval);
+		break;
 		case SYS_write:
 		err = sys_write(tf->tf_a0,
 				 	(void*) tf->tf_a1,
